0b58c01a9cf05b6c2b6ec2ea7adf0b4a97262cc6
Bugs Bunny <jiri.zajic@mensa.cz>
2017-11-19 15:32:15 +0100

    Two charts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@gooddata/react-components/styles/css/main.css';
+import { AfmComponents } from '@gooddata/react-components';
+import catalogJson from './catalog.json';
+
+const { ColumnChart } = AfmComponents;
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    getAfmMeasures() {
+        return {
+            measures: [{
+                id: '# of Activities',
+                definition: {
+                    baseObject: {
+                        id: catalogJson.metrics['# of Activities'].identifier
+                    }
+                }
+            }]
+        }
+    }
+
+    getAfm() {
+        return {
+            ...this.getAfmMeasures(),
+            filters: [{
+                between: [-1, -1],
+                granularity: 'year',
+                id: catalogJson.dateDataSets['Date (Activity)'].identifier,
+                intervalType: 'relative',
+                type: 'date'
+            }]
+        };
+    }
+
+    getAfmForAllYears() {
+        return {
+            ...this.getAfmMeasures(),
+            attributes: [{
+                id: catalogJson.dateDataSets['Date (Activity)'].attributes['Year (Activity)'].defaultDisplayForm.identifier,
+                type: 'attribute'
+            }]
+        }
+    }
+
+    renderDropdown() {
+        return (
+            <select defaultValue="2016">
+                <option value="2018">2018</option>
+                <option value="2017">2017</option>
+                <option value="2016">2016</option>
+                <option value="2015">2015</option>
+            </select>
+        )
+    }
+
+    render() {
+        const projectId = 'la84vcyhrq8jwbu4wpipw66q2sqeb923';
+        const afm = this.getAfm();
+        const afmAllYears = this.getAfmForAllYears();
+
+        return (
+            <div className="App">
+                <h1># of Activities: Year {this.renderDropdown()}</h1>
+                <div>
+                    <ColumnChart
+                        afm={afm}
+                        projectId={projectId}
+                    />
+                </div>
+                <h1># of Activities: Overview (Year by Year)</h1>
+                <div>
+                    <ColumnChart
+                        afm={afmAllYears}
+                        projectId={projectId}
+                    />
+                </div>
+         </div>
+      );
+   }
 }
 
 export default App;
